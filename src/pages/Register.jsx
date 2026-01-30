d4ce56461517f0bb3c4bafedcf207d8ab6e37041
milindkyada <milindkyada@gmail.com>
2026-01-30 13:48:20 +0530

    register page added

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css";
+const Register = () => {
+  return (
+    <>
+    <div className="form-container">
+    <h1>Register</h1>
+    </div>
+    </>
+  );
+};
+
+export default Register;
